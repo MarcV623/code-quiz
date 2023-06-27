19e8270f3b6ea0d698f0df97008d383ac857dd43
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-06-27 12:42:23 -0500

    added questions & answers on page

--- script.js
+++ script.js
@@ -1,5 +1,59 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Quiz Questions
+const questions = [
+  {
+    question: "What is javascript?",
+    answers: [
+      { answer: "type of latte", correct: false },
+      { answer: "scripting language", correct: true },
+      { answer: "a poem", correct: false },
+      { answer: "an object", correct: false }
+    ]
+  },
+  {
+    question: "commonly used data types DO NOT include:",
+    answers: [
+      { answer: "strings", correct: false },
+      { answer: "alerts", correct: false },
+      { answer: "Booleans", correct: true },
+      { answer: "numbers", correct: false }
+    ]
+  },
+  {
+    question: "an array in javascript can be used to store?",
+    answers: [
+      { answer: "number of strings", correct: false },
+      { answer: "other arrays", correct: false },
+      { answer: "Booleans", correct: false },
+      { answer: "all of the above", correct: true }
+    ]
+  },
+  {
+    question: "Which HTML elements do we put in javascript code?",
+    answers: [
+      { answer: "<script>", correct: true },
+      { answer: "<js>", correct: false },
+      { answer: "<body>", correct: false },
+      { answer: "<head>", correct: false }
+    ]
+  }
+]
+
+// Header
+const questionText = document.querySelector("#question-placeholder")
+
+// Buttons
+const answerABtn = document.querySelector("#answer-a") 
+const answerBBtn = document.querySelector("#answer-b") 
+const answerCBtn = document.querySelector("#answer-c") 
+const answerDBtn = document.querySelector("#answer-d")
+
+// Update page with first question
+questionText.innerHTML = questions[0].question
+answerABtn.innerHTML = questions[0].answers[0].answer
+answerBBtn.innerHTML = questions[0].answers[1].answer
+answerCBtn.innerHTML = questions[0].answers[2].answer
+answerDBtn.innerHTML = questions[0].answers[3].answer
+
 
 // Generate password
 function generatePassword(length, lowercase_flag, uppercase_flag, numeric_flag, special_flag) {
