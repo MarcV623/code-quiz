f2fe3940e872669fefb679b8ca7f6e8b0b396fa6
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-06-28 13:03:19 -0500

    added code to change question

--- script.js
+++ script.js
@@ -38,6 +38,9 @@ const questions = [
   }
 ]
 
+// Current question
+let current
+
 // Header
 const questionText = document.querySelector("#question-placeholder")
 
@@ -47,6 +50,17 @@ const answerBBtn = document.querySelector("#answer-b")
 const answerCBtn = document.querySelector("#answer-c") 
 const answerDBtn = document.querySelector("#answer-d")
 
+// Handle button clicks
+function handleClick() {
+  current = questions.shift()
+
+  questionText.innerHTML = current.question
+  answerABtn.innerHTML = current.answers[0].answer
+  answerBBtn.innerHTML = current.answers[1].answer
+  answerCBtn.innerHTML = current.answers[2].answer
+  answerDBtn.innerHTML = current.answers[3].answer
+}
+
 // Added eventListener() to buttons
 answerABtn.addEventListener("click", handleClick)
 answerBBtn.addEventListener("click", handleClick)
@@ -54,93 +68,4 @@ answerCBtn.addEventListener("click", handleClick)
 answerDBtn.addEventListener("click", handleClick)
 
 // Update page with first question
-questionText.innerHTML = questions[0].question
-answerABtn.innerHTML = questions[0].answers[0].answer
-answerBBtn.innerHTML = questions[0].answers[1].answer
-answerCBtn.innerHTML = questions[0].answers[2].answer
-answerDBtn.innerHTML = questions[0].answers[3].answer
-
-
-// Generate password
-function generatePassword(length, lowercase_flag, uppercase_flag, numeric_flag, special_flag) {
-  const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  const numeric = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-  const special = ['*', '$', '#', '%', '&', "#", '!', '^']
-
-  let password = ''
-  let pool = []
-
-  if (typeof length !== "number") {
-    return "error: length is not a number"
-  }
-
-  if (length < 8) {
-    return "error: length is less than 8"
-  }
-
-  if (length > 128) {
-    return "error: length is greater than 128"
-  }
-
-  if (typeof lowercase_flag !== "boolean") {
-    return "error: lowercase_flag is not a boolean"
-  }
-
-  if (typeof uppercase_flag !== "boolean") {
-    return "error: uppercase_flag is not a boolean"
-  }
-
-  if (typeof numeric_flag !== "boolean") {
-    return "error: numeric_flag is not a boolean"
-  }
-
-  if (typeof special_flag !== "boolean") {
-    return "error: special_flag is not a boolean"
-  }
-
-  if (!lowercase_flag && !uppercase_flag && !numeric_flag && !special_flag) {
-    return "error: no character sets selected"
-  }
-    
-  if (lowercase_flag) {
-    pool = pool.concat(lowercase)
-  }
-
-  if (uppercase_flag) {
-    pool = pool.concat(uppercase)
-  }
-  
-  if (numeric_flag) {
-    pool = pool.concat(numeric)
-  }
-
-  if (special_flag) {
-    pool = pool.concat(special)
-  }
-
-  for (let i = 0; i < length; i++) {
-    password = password + pool[Math.floor(Math.random() * pool.length)]
-  }
-
-  return password
-}
-
-// Write password to the #password input
-function writePassword() {
-  let passwordlength = parseInt(document.querySelector("#passwordlength").value)
-  let lowercase_flag = document.querySelector("#lowercase_flag").checked
-  let uppercase_flag = document.querySelector("#uppercase_flag").checked
-  let numeric_flag = document.querySelector("#numeric_flag").checked
-  let special_flag = document.querySelector("#special_flag").checked
-  
-  var password = generatePassword(passwordlength, lowercase_flag, uppercase_flag, numeric_flag, special_flag);
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-function handleClick() {
-  console.log("Hello World!")
-}
-
+handleClick()
